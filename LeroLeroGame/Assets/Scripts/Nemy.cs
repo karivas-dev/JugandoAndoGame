00516c05cbb0c827113a74dc0c27bc5f5aaa4b10
Alca <karenrivasg17@gmail.com>
2023-05-21 21:18:33 -0600

    Basic movement and animations

---
+++ LeroLeroGame/Assets/Scripts/Nemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nemy : MonoBehaviour
+{
+    /*private Animator animator;
+    public Rigidbody2D rigidbody2D;
+    public Transform player;
+    private bool lookRigth = true;
+
+    [Header("HP")]
+    [SerializeField] private float vida;
+    [SerializeField] private BarraDeVida barraDeVida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LookAtPlayer() 
+    {
+        if ((player.position.x > transform.position.x && !lookRigth))
+        {
+
+        }
+    }*/
+}
---
+++ LeroLeroGame/Assets/Scripts/Player/PlayerPlatformerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Use this for initialization
+    void Awake () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer> ();    
+        animator = GetComponent<Animator> ();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis ("Horizontal");
+
+        if (Input.GetButtonDown ("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        } else if (Input.GetButtonUp ("Jump")) 
+        {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.0f) : (move.x < 0.0f));
+        if (flipSprite) 
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool ("grounded", grounded);
+        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+
+
+        targetVelocity = move * maxSpeed;
+        Debug.Log(grounded);
+
+        /*
+         Debug.Log("XD");
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+
+        if (Horizontal != 0 || Vertical != 0 ) {
+            animator.SetFloat("Horizontal", Horizontal);
+            //animator.SetFloat("Vertical", Vertical);
+            animator.SetFloat("Speed", 1);
+        } else {
+            animator.SetFloat("Speed", 0);
+        }*/
+    }
+}
