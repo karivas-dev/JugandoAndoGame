52aec64c5a8e265db7fcb649bc061896b0ade74b
Luis Bonilla <124472850+bocchiko@users.noreply.github.com>
2023-06-05 11:50:09 -0600

    Added Player Prefab for World Map Scene

---
+++ LeroLeroGame/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,94 @@
+//using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] public float moveSpeed = 5f;
+    public float collisionOffset = 0.02f;
+    public ContactFilter2D movementFilter;
+
+    Vector2 movementInput;
+    SpriteRenderer sprite;
+    Animator animator;
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    void Start()
+    {
+        rb =  GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        movementInput =  new Vector2(moveX, moveY).normalized;
+
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetFloat("Speed", movementInput.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        //Character movement
+        if(movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+
+            if(!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+            }
+            if(!success)
+            {
+                success = TryMove(new Vector2(0, movementInput.y));
+            }
+
+            animator.SetBool("isMoving", success);
+
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        if(direction != Vector2.zero)
+        {
+            int count = rb.Cast(
+                direction,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+            
+            if(count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+}
