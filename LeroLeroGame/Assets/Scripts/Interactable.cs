8c58dc773106048b661c4dbd790367be76625fec
Luis Bonilla <124472850+bocchiko@users.noreply.github.com>
2023-06-06 16:29:37 -0600

    Added Interactable Key Items

--- LeroLeroGame/Assets/Scripts/Interactable.cs
+++ LeroLeroGame/Assets/Scripts/Interactable.cs
@@ -2,65 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
-using TMPro;
 
 public class Interactable : MonoBehaviour
 {
-	public UnityEvent interactAction;
-	public bool isInRange = false;
-	public bool isTriggered = false;
-	public KeyCode interactKey;
-	public GameObject dialogBox;
-	public GameObject player;
-	public GameObject recurso;
-	public Sprite[] sprites;
-	int spriteIndex = 0;
+    public UnityEvent interactAction;
+    public KeyCode interactKey;
+    public GameObject dialogBox;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		sprites = (UnityEngine.Sprite[])Resources.LoadAll("Sprites");
-	}
+    public List<Sprite> sprites = new List<Sprite>();
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (isInRange)
-		{
-			if (Input.GetKeyDown(interactKey) && isTriggered == false)
-			{
-				interactAction.Invoke();
-				dialogBox.SetActive(true);
-				isTriggered = true;
-				this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[spriteIndex];
-				spriteIndex++;
-				if (spriteIndex == 4)
-				{
-					spriteIndex = -1;
-				}
-				
-			}
-			else if (Input.GetKeyDown(interactKey) && isTriggered)
-			{
-				dialogBox.SetActive(false);
-				isTriggered = false;
+    private bool isInRange = false;
+    private int spriteIndex = 0;
 
-			}
-		}
-	}
+    private SpriteRenderer spriteRenderer;
 
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.gameObject.CompareTag("Player"))
-			isInRange = true;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
+    private void Update()
+    {
+        if (isInRange && Input.GetKeyDown(interactKey))
+        {
+            if (spriteIndex < sprites.Count)
+            {
+                ChangeSprite();
+            }
+            else
+            {
+                isInRange = false;
+            }
+        }
+    }
 
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
 
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-		if (collision.gameObject.CompareTag("Player"))
-			isInRange = false;
-	}
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+
+    private void ChangeSprite()
+    {
+        spriteRenderer.sprite = sprites[spriteIndex];
+        spriteIndex++;
+        interactAction.Invoke();
+        dialogBox.SetActive(true);
+    }
 }
+
+
+
+
