afe11b380e234315781e20f9abbf262e05cbb537
Luis Bonilla <124472850+bocchiko@users.noreply.github.com>
2023-06-11 20:28:45 -0600

    Revert "Unlocking levels system"
    This reverts commit c2950d3bbca0b6513f7613c0410d7ff8a897dbfd.

--- LeroLeroGame/Assets/Scripts/MapScripts/BuildingTrigger.cs
+++ LeroLeroGame/Assets/Scripts/MapScripts/BuildingTrigger.cs
@@ -1,72 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildingTrigger : MonoBehaviour
 {
-    public bool isInRange = false;
-    public KeyCode interactKey;
+	public bool isInRange = false;
+	public KeyCode interactKey;
     public int buildingNumber;
 
-    private void Update()
-    {
-        if (isInRange)
-        {
-            if (Input.GetKeyDown(interactKey))
-            {
-                LoadLevel(buildingNumber);
-            }
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (isInRange)
+		{
+			if (Input.GetKeyDown(interactKey))
+			{
+				switch(buildingNumber)
+                {
+                    case 1:
+                        Loader.Load(Loader.Scene.CopyRight);
+                        break;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-            isInRange = true;
-    }
+                    case 2:
+						Loader.Load(Loader.Scene.IndustrialProperty);
+						break;
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-            isInRange = false;
-    }
+                    case 3:
+                        break;
+                }
+			}
+		}
+	}
 
-    private void LoadLevel(int level)
-    {
-        if (PlayerPrefs.GetInt("BuildingNumber") >= level)
-        {
-            switch (level)
-            {
-                case 1:
-                    Loader.Load(Loader.Scene.CopyRight);
-                    break;
-                case 2:
-                    Loader.Load(Loader.Scene.IndustrialProperty);
-                    break;
-                // Agrega más casos para niveles adicionales según sea necesario
-            }
-        }
-        else
-        {
-            Debug.Log("No puedes acceder a este nivel todavía");
-        }
-    }
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+			isInRange = true;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+			isInRange = false;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
