85c740d4f36ca8ba9acd273b7154e30f1ac68f9b
Cotto213 <85969266+Duque206@users.noreply.github.com>
2023-06-12 22:49:55 -0600

    Key elements update
    Se añadió las definiciones con conceptos para los elementos al interactuar con ellos, de igual manera un contador para cada vez que se interactue.

--- LeroLeroGame/Assets/Scripts/MuseumScripts/Interactable.cs
+++ LeroLeroGame/Assets/Scripts/MuseumScripts/Interactable.cs
@@ -7,6 +7,7 @@ using TMPro;
 
 public class Interactable : MonoBehaviour
 {
+	private static int interactions = 0;
 	public KeyCode interactKey;
 	public TMP_Text textElement;
 	public GameObject dialogBox;
@@ -14,12 +15,17 @@ public class Interactable : MonoBehaviour
 	public List<Sprite> sprites = new List<Sprite>();
 	private bool isInRange = false;
 	private int spriteIndex = 0;
-	public string[] textDefinitions = {"s",
-	"d",
-	"NO","jaj", "r", 
-	};
+	public static string[] textDefinitions = {"¡Mira lo que has creado! Es una hermosa pintura de un paisaje. Al crear esta obra, estás ejerciendo tu derecho de autor, que te protege como el creador original de esta pintura. Esto significa que nadie puede copiarla o usarla sin tu permiso.",
+	"¡Increíble! Has creado una pintura abstracta. Al expresar tus ideas y emociones a través de esta obra, estás demostrando tu propiedad intelectual. Esto significa que tienes derechos exclusivos sobre esta creación artística y puedes decidir cómo compartirla y protegerla.",
+	" ¡Vaya! Has creado un retrato muy realista. Esta obra de arte está protegida por los derechos de autor. Esto significa que nadie puede copiarla o vender reproducciones sin tu autorización. Eres el dueño de esta creación y puedes decidir cómo se utiliza.",
+	"¡Genial! Has creado una pintura de un animal. Al hacerlo, estás mostrando tu talento artístico y tus habilidades creativas. Recuerda que esta pintura está protegida por los derechos de autor, lo que te da el poder de decidir cómo se utiliza y quién puede beneficiarse de ella.",
+	"¡Fascinante! Has creado una escultura abstracta. Al ser el creador de esta obra, tienes derechos de propiedad intelectual sobre ella. Esto significa que puedes decidir cómo se comparte, se exhibe y se protege. Tu creatividad es valiosa y debes asegurarte de que se respete.",
+	"¡Asombroso! Has creado un plano industrial de una fábrica. Este plano es parte de la propiedad industrial, que se refiere a los derechos sobre invenciones y creaciones relacionadas con la industria y el comercio. Al diseñar este plano, estás mostrando tu talento para desarrollar ideas prácticas y eficientes.",
+	"¡Mira lo que hiciste! Has creado una escultura de una figura humana. Al crear esta obra de arte, estás ejerciendo tu propiedad intelectual. Puedes registrar esta escultura para obtener una protección adicional y asegurarte de que nadie pueda copiarla o utilizarla sin tu permiso."
+  };
 	private bool isDialogActive = false;
 
+	[SerializeField]private  Text count;
 
 	private bool hasInteracted = false;
 	private bool dialogIn = false;
@@ -33,6 +39,7 @@ public class Interactable : MonoBehaviour
 
 	private void Update()
 	{
+		
 		if (isInRange && Input.GetKeyDown(interactKey))
 		{
 			if (spriteIndex < sprites.Count)
@@ -44,6 +51,7 @@ public class Interactable : MonoBehaviour
 				player.GetComponent<Movement>().enabled = false;
 				dialogBox.SetActive(true);
 				isDialogActive = true;
+				textIndex++;
 			}
 			else if (isDialogActive)
 			{
@@ -79,6 +87,13 @@ public class Interactable : MonoBehaviour
 	{
 		spriteRenderer.sprite = sprites[spriteIndex];
 		spriteIndex++;
+
+		if (spriteIndex == sprites.Count)
+		{
+			interactions++;
+			count.text = "Objetos creados: " + interactions + "/8";
+		}
 	}
 }
+
 
