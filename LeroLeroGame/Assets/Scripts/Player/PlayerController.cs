73b1d3ac3652538c52a09c71d6d471fdcd7d1106
Luis Bonilla <124472850+bocchiko@users.noreply.github.com>
2023-06-16 21:21:06 -0600

    Idle Left Animation for MapWorld

--- LeroLeroGame/Assets/Scripts/Player/PlayerController.cs
+++ LeroLeroGame/Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,7 @@
-//using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 
 public class PlayerController : MonoBehaviour
 {
@@ -16,10 +14,11 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     Rigidbody2D rb;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    bool isFacingLeft = false;
 
     void Start()
     {
-        rb =  GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
     }
@@ -28,50 +27,56 @@ public class PlayerController : MonoBehaviour
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        movementInput =  new Vector2(moveX, moveY).normalized;
+        movementInput = new Vector2(moveX, moveY).normalized;
 
-        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Horizontal", Mathf.Abs(moveX));
         animator.SetFloat("Vertical", moveY);
         animator.SetFloat("Speed", movementInput.sqrMagnitude);
+
+        if (moveX < 0 && !isFacingLeft)
+        {
+            FlipCharacter();
+        }
+        else if (moveX > 0 && isFacingLeft)
+        {
+            FlipCharacter();
+        }
     }
 
     private void FixedUpdate()
     {
-        //Character movement
-        if(movementInput != Vector2.zero)
+        if (movementInput != Vector2.zero)
         {
             bool success = TryMove(movementInput);
 
-            if(!success)
+            if (!success)
             {
                 success = TryMove(new Vector2(movementInput.x, 0));
             }
-            if(!success)
+            if (!success)
             {
                 success = TryMove(new Vector2(0, movementInput.y));
             }
 
             animator.SetBool("isMoving", success);
-
         }
         else
         {
             animator.SetBool("isMoving", false);
         }
-        
     }
 
     private bool TryMove(Vector2 direction)
     {
-        if(direction != Vector2.zero)
+        if (direction != Vector2.zero)
         {
             int count = rb.Cast(
                 direction,
                 movementFilter,
                 castCollisions,
                 moveSpeed * Time.fixedDeltaTime + collisionOffset);
-            
-            if(count == 0)
+
+            if (count == 0)
             {
                 rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
                 return true;
@@ -91,4 +96,13 @@ public class PlayerController : MonoBehaviour
     {
         movementInput = movementValue.Get<Vector2>();
     }
+
+    private void FlipCharacter()
+    {
+        isFacingLeft = !isFacingLeft;
+        sprite.flipX = isFacingLeft;
+    }
 }
+
+
+
