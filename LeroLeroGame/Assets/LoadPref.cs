9b7f3fbf2e544bdcef65cec0806206b6f79672cf
Cotto213 <85969266+Duque206@users.noreply.github.com>
2023-06-18 22:43:06 -0600

    Mapworld 2 updated

---
+++ LeroLeroGame/Assets/LoadPref.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadPref : MonoBehaviour
+{
+	List<string> completedElements = new List<string>();
+	public List<SpriteRenderer> sprites = new List<SpriteRenderer>();
+	public List<Image> imageComponents = new List<Image>();
+	private void Awake()
+	{
+		LoadInteractions();
+		// Otro código de inicialización
+	}
+
+	private void Start()
+	{
+		foreach (var element in completedElements)
+		{
+			// Find the GameObject with the corresponding name
+			GameObject elementObject = GameObject.Find(element);
+
+			// Check if the GameObject exists
+			if (elementObject != null)
+			{
+				// Get the SpriteRenderer component of the GameObject
+				SpriteRenderer spriteRenderer = elementObject.GetComponent<SpriteRenderer>();
+				Image image = elementObject.GetComponent<Image>();
+
+				// Check if the SpriteRenderer component exists
+				if (spriteRenderer != null)
+				{
+					// Enable the sprite renderer
+					spriteRenderer.enabled = true;
+					image.enabled = true; 
+
+
+					Debug.Log("Element: " + element + ", SpriteRenderer Enabled");
+				}
+				else
+				{
+					Debug.Log("SpriteRenderer component not found for element: " + element);
+				}
+			}
+			else
+			{
+				Debug.Log("Element GameObject not found: " + element);
+			}
+		}
+	}
+
+
+
+
+
+
+	private void LoadInteractions()
+	{
+		string interactionString = PlayerPrefs.GetString("Interactions", "");
+		string[] interactionArray = interactionString.Split(',');
+		completedElements = new List<string>(interactionArray);
+	}
+}
---
+++ LeroLeroGame/Assets/ToggleCanvas.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ToggleCanvas : MonoBehaviour
+{
+	public Canvas canvas;
+	public KeyCode hideKey = KeyCode.X;
+	public bool isactive = true;
+	public GameObject player;
+
+
+	private void Start()
+	{
+		player.GetComponent<PlayerController>().enabled = false;
+	}
+	private void Update()
+	{
+			
+			if (Input.GetKeyDown(hideKey))
+			{
+			player.GetComponent<PlayerController>().enabled = true;
+			canvas.gameObject.SetActive(false);
+			}
+	
+		
+	}
+}
+
+
